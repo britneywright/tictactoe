d3b4e7f0ab4fa1020fd7381103120a43be2b84c8
Britney Wright <blw06g@gmail.com>
2014-06-26 11:31:41 -0400

    added player tests for name and color getter methods

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,8 +8,22 @@ module TicTacToe
       end
 
       it "does not raise an error when initialized with a valid input" do
-        input = { color: "X", name: "Someone"}
-        expect { Player.new(input)}.to_not raise_error
+        input = {color: "X", name: "Someone"}
+        expect {Player.new(input)}.to_not raise_error
+      end
+    end
+    context "#color" do
+      it "returns the color" do
+        input = {color: "X", name: "Someone"}
+        player = Player.new(input)
+        expect(player.color).to eq "X"
+      end
+    end
+    context "#name" do
+      it "returns the player's name" do
+        input = {color: "X", name: "Someone"}
+        player = Player.new(input)
+        expect(player.name).to eq "Someone"
       end
     end
   end
