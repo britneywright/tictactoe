68cce7bf21c783ca4f79535f9150dbc919c235cd
Britney Wright <blw06g@gmail.com>
2014-06-27 22:08:23 -0400

    changed 'have(n).items' tests to '(collection.size).to eq(n)'

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,6 +6,16 @@ module TicTacToe
       it "initializes the board with a grid" do
         expect { Board.new(grid: "grid") }.to_not raise_error
       end
+      it "sets the grid with three rows by default" do
+        board = Board.new
+        expect(board.grid.size).to eq(3)
+      end
+      it "creates three things in each row by default" do
+        board = Board.new
+        board.grid.each do |row|
+          expect(row.size).to eq(3)
+        end
+      end
     end
   end
 end
