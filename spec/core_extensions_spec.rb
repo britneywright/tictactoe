929bb0100960787acf74db09cc749f6e95f0ce1c
Britney Wright <blw06g@gmail.com>
2014-07-03 09:56:33 -0400

    wrote tests for all_same? method

--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -14,4 +14,18 @@ describe Array do
       expect([].all_empty?).to eq true
     end
   end
+
+  context "#all_same?" do
+    it "returns true if all elements of the Array are the same" do
+      expect(["A", "A", "A"].all_same?).to eq true
+    end
+
+    it "returns false if some of the Array elements are not the same" do
+      expect(["", 1, "", Object.new, :a].all_same?).to eq false
+    end
+
+    it "returns true for an empty Array" do
+      expect([].all_same?).to eq true
+    end
+  end
 end
